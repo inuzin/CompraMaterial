c4491ece0e72c528f1526bc401ca856dd3b5099f
unknown <left4you@hotmail.com>
2021-04-29 09:34:51 -0300

    Fix

--- src/Cadastro.java
+++ src/Cadastro.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 
 public class Cadastro 
 {
-	private int quantidade_total;
-	private float valor_total;
+	public int quantidade_total;
+	public float valor_total;
 	
 	public void AddProduto(ArrayList<Produto> p, String nome, String descricao, String medida, double valormedida)
 	{
@@ -15,12 +15,20 @@ public class Cadastro
 		s.add(new Solicitacao(new ArrayList<Item>(), pagamento, formapagamento, localcompra, datacompra, status));
 	}
 	
-	public void AtualizarSolicitacao(Solicitacao s, String pagamento, String formapagamento, String localcompra, String datacompra)
+	public void AtualizarSolicitacao(Solicitacao s, String Item_Nome, int Quantidade, String pagamento, String formapagamento, String localcompra, String datacompra)
 	{
+		//adicionar item -- checar se ja existe primeiro
+		s.item.get(s.item.size() -1).P.setNome(Item_Nome);
+		s.item.get(s.item.size() -1).quantidade += 1;
 		s.pagamento = pagamento;
 		s.formapagamento = formapagamento;
 		s.localcompra = localcompra;
 		s.datacompra = datacompra;
+	}
+	
+	public void AddItem(ArrayList<Produto> p, Solicitacao s, String Nome, int Quantidade)
+	{
+		//Checa se o produto existe
 	}
 	
 	public void CancelarSolicitacao(Solicitacao s)
@@ -35,9 +43,19 @@ public class Cadastro
 		{
 			for(int j = 0; j <= s.get(i).item.size() -1; j++)
 			{
-				Console.writeline("" + s.get(i).item.get(j).P.getNome());	
+				//Retornar nome de cada produto
+				System.out.println("" + s.get(i).item.get(j).P.getNome());
+				//Somar preco
+				quantidade_total += s.get(i).item.get(j).P.getValormedida();
 			}			
 		}
+	}
+	
+	public ArrayList<Solicitacao> FilteredList(ArrayList<Solicitacao> s)
+	{
+		//Forma uma nova lista para evitar repeticoes 
+		
+		return s;
 	}
 
 }
--- src/main.java
+++ src/main.java
@@ -2,15 +2,40 @@ import java.awt.List;
 import java.util.ArrayList;
 
 public class main {
-	ArrayList<Produto> produtos = new ArrayList();
-	ArrayList<Solicitacao> solicitacoes = new ArrayList();
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
-        Console.writeline("Escolha a opcao desejada: ");
-        Console.writeline("1- Adicionar Produto");
-        Console.writeline("2- Adicionar Solicitacao");
+		ArrayList<Produto> produtos = new ArrayList<Produto>();
+		ArrayList<Solicitacao> solicitacoes = new ArrayList<Solicitacao>();		
+		Cadastro cadastro = new Cadastro();
+		
+		cadastro.AddProduto(produtos, "Madeira", "Material para casa", "Cm", 7.49f);
+		cadastro.AddProduto(produtos, "Pedra", "Material para casa", "", 2.53f);
+		cadastro.AddProduto(produtos, "Areia", "Material para casa", "", 5.27f);
+		
+		ListarProdutos(produtos);
+	}
+	
+	public static void ListarProdutos(ArrayList<Produto> p)
+	{
+		for(int i = 0; i <= p.size() - 1; i++)
+		{
+			System.out.println("" + p.get(i).getNome());
+			System.out.println("      Desc: " + p.get(i).getDescricao());
+			System.out.println("      Medida: " + p.get(i).getMedida());
+			System.out.println("      Valor por medida: " + p.get(i).getValormedida());
+		}
+	}
+	
+	public static void ListarSolicitacoes(ArrayList<Solicitacao> s)
+	{
+		for(int i = 0; i <= s.size() - 1; i++)
+		{
+			System.out.println("" + s.get(i).getPagamento());
+			System.out.println("      Desc: " + s.get(i).getDescricao());
+			System.out.println("      Medida: " + p.get(i).getMedida());
+			System.out.println("      Valor por medida: " + p.get(i).getValormedida());
+		}
 	}
 
 }
